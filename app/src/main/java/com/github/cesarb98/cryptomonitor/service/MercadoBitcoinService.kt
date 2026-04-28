60ebfd89acaf4a2041045ce0e075d98bfccc4fbe
Cesar Brasil Alves <cesarbrasilalves@gmail.com>
2026-04-28 13:16:33 -0300

    Aplicativo finalizado
    git push origin git push origin mastermaster#

--- app/src/main/java/com/github/cesarb98/cryptomonitor/service/MercadoBitcoinService.kt
+++ app/src/main/java/com/github/cesarb98/cryptomonitor/service/MercadoBitcoinService.kt
@@ -1,12 +1,10 @@
 package com.github.cesarb98.cryptomonitor.service
 
-
 import com.github.cesarb98.cryptomonitor.model.TickerResponse
-
-
+import retrofit2.Response
+import retrofit2.http.GET
 
 interface MercadoBitcoinService {
-
 
     @GET("api/BTC/ticker/")
     suspend fun getTicker(): Response<TickerResponse>
